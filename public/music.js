4d42b03941423c4ae1b1e3356575f22db5650b90
MatthewHightech <Smoose007@gmail.com>
2020-09-30 19:07:31 -0700

    piano player

---
+++ public/music.js
@@ -0,0 +1,66 @@
+const audioContext = new AudioContext();
+
+// Select the Audio Element from the DOM
+const htmlAudioElement = document.getElementById("audio");
+// Create an "Audio Node" from the Audio Element
+const source = audioContext.createMediaElementSource(htmlAudioElement);
+// Connect the Audio Node to your speakers. Now that the audio lives in the
+// Audio Context, you have to explicitly connect it to the speakers in order to
+// hear it
+source.connect(audioContext.destination);
+
+if (typeof Meyda === "undefined") {
+    alert("Meyda could not be found! Have you included it?");
+  }
+
+// Create the Meyda Analyzer
+const analyzer = Meyda.createMeydaAnalyzer({
+    // Pass in the AudioContext so that Meyda knows which AudioContext Box to work with
+    "audioContext": audioContext,
+    // audio node
+    "source": source,
+    // how often to check the audio sample (44100/512 = 86 times a sec *average*)
+    "bufferSize": 2048,
+    // Different audio features to calculate
+    "featureExtractors": ["rms", "chroma", "spectralCentroid"], 
+    // Calculates data and adds it to features everytime the callback runs (86x/sec)
+    "callback": features => {
+        console.log(features);
+        piano(features.chroma); 
+    }
+});
+
+// starts analyzer and audio track on button click
+document.querySelector('#start').addEventListener('click', function() {
+    audioContext.resume().then(() => {
+        console.log('Playback started successfully');
+        analyzer.start();
+        htmlAudioElement.play(); 
+    });
+});
+
+// stops analyzer and audio track on button click
+document.querySelector('#stop').addEventListener('click', function() {
+    audioContext.resume().then(() => {
+        console.log('Playback stopped successfully');
+        analyzer.stop();
+        htmlAudioElement.pause(); 
+    });
+});
+
+function piano(chroma) {
+    for(let i = 0; i < chroma.length; i++) {
+        let key = document.getElementById(i); 
+        if (chroma[i] == 1) {
+            key.style.backgroundColor = "green";  
+        } else {
+            if (key.className == "regular") {
+                key.style.backgroundColor = "white";
+            } else {
+                key.style.backgroundColor = "black";
+            }
+            
+        }
+    }
+}
+
--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
-const net = new brain.NeuralNetwork({
+const net = new brain.recurrent.LSTM({
     activation: 'sigmoid', // activation function
     hiddenLayers: [2],
-    iterations: 20000,
+    iterations: 2000,
     learningRate: 0.5 // global learning rate, useful when training using streams
   });
 
-//net.train(data); 
+net.train(data); 
 const input = document.getElementById('name');
 const submit = document.getElementById('submit');
 const greeting = document.getElementById('greeting'); 
@@ -13,21 +13,24 @@ const maleButton = document.getElementById('male-button');
 const femaleButton = document.getElementById('female-button');
 const printButton = document.getElementById('print-button');
 
-const gender = "Mrs. "; 
+let gender = "Mrs. "; 
+let gName = ""; 
 
 // clicks submit button and displays text
 submit.addEventListener('click', () => {
+    gName = input.value.toLowerCase(); 
+    gender = guess(gName); 
     greeting.innerHTML = 'Hello ' + gender + input.value; 
   })
 
 // selects male name
 maleButton.addEventListener('click', () => {
-  enterData("male")
+  enterData(1); 
 });
 
 // selects female name
 femaleButton.addEventListener('click', () => {
-    enterData("female")
+    enterData(0);
 });
 
 // prints data in console
@@ -41,10 +44,28 @@ function print() {
 // enters data into array
 function enterData(value) {
     data.push({
-        input: color,
-        output: [value]
-    })
+        input: gName,
+        output: value
+    }); 
     
 } // enterData
 
+// runs the net to guess male or female
+function guess(name) {
+    const netGuess = net.run(name)[0]; 
+    console.log(netGuess); 
+    return netGuess > 0.5 ? "Mr. " : "Mrs. "; 
+} // guess
+/*
+// returns the name in an object of it's letters
+function getNameObj(name) {
+    let nameObj = new Object();
+    for (let i = 0; i < 10; i++) {
+        let currentLetter = name.charAt(i);
+        nameObj[i] = currentLetter; 
+        console.log(nameObj[i]); 
+    }
+    return nameObj;
+} // getNameObj
+*/
 
